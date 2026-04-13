1e2389507f7bb0568b4a80313cf28b850d5549c7
Jorge Sanchez <jorge@vionta.net>
2026-04-14 01:10:07 +0200

    Updates

--- src/main/java/net/vionta/xml/fento/repository/impl/ClassPathRepository.java
+++ src/main/java/net/vionta/xml/fento/repository/impl/ClassPathRepository.java
@@ -2,30 +2,28 @@ package net.vionta.xml.fento.repository.impl;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
-import java.io.File;
 import java.io.Serializable;
 
 import org.slf4j.Logger;
 import org.w3c.dom.Document;
 
-import net.vionta.xml.fento.bind.serialize.Deserialzer;
+import net.vionta.xml.fento.bind.serialize.Deserializer;
 import net.vionta.xml.fento.bind.serialize.Serializer;
 import net.vionta.xml.fento.repository.DocumentRepository;
 import net.vionta.xml.fento.repository.exception.PersistException;
 import net.vionta.xml.fento.repository.exception.RetrieveException;
 import net.vionta.xml.fento.repository.impl.util.ClasspathFileUtil;
 import net.vionta.xml.fento.repository.impl.util.DocumentUtils;
-import net.vionta.xml.fento.repository.impl.util.FileManager;
 import net.vionta.xml.fento.repository.impl.util.PathAdjust;
 
-public class ClassPathRepository implements DocumentRepository {
+public class ClassPathRepository implements DocumentRepository  {
 	
 	private String path ;
 	
 	static Logger log  = getLogger(ClassPathRepository.class);
 
 	@Override
-	public Object load(Serializable object) throws RetrieveException {
+	public <T extends Serializable> T load(T object) throws RetrieveException {
 		try {
 			// Calculate Path.
 			String adjustedPath = PathAdjust.adjustedPath(getPath(), object);
@@ -33,7 +31,7 @@ public class ClassPathRepository implements DocumentRepository {
 			String fileContent = ClasspathFileUtil.readFile(adjustedPath);
 			log .debug("File Content:"+fileContent);
 			Document documentContents = DocumentUtils.stringToDocument(fileContent);
-			return  new Deserialzer().deserialize(object, documentContents);
+			return  (T) new Deserializer().deserialize(object, documentContents);
 		} catch (Exception e) {
 			log .error(e.toString());
 			RetrieveException re = new RetrieveException();
@@ -90,13 +88,6 @@ public class ClassPathRepository implements DocumentRepository {
 		}
 	}
 
-	
-	@Override
-	public Object template() {
-		// m
-		return null;
-	}
-
 	public String getPath() {
 		return path;
 	}
@@ -104,6 +95,16 @@ public class ClassPathRepository implements DocumentRepository {
 	public ClassPathRepository setPath(String path) {
 		this.path = path;
 		return this;
+	}
+
+	@Override
+	public void remove(Serializable object) throws RetrieveException {
+		throw new IllegalStateException("Classpath repositories should not remove files.");
+	}
+
+	@Override
+	public Document template() {
+		throw new IllegalStateException("Classpath repositories should not create documents from templates.");
 	}
 	
 }
