250f6029ec10c5b6db7faa17091aa3b91d74e210
Jorge Sanchez <jorge@vionta.net>
2026-04-14 01:10:06 +0200

    Updates

--- src/main/java/net/vionta/xml/fento/repository/DocumentRepository.java
+++ src/main/java/net/vionta/xml/fento/repository/DocumentRepository.java
@@ -7,14 +7,12 @@ import org.w3c.dom.Document;
 import net.vionta.xml.fento.repository.exception.PersistException;
 import net.vionta.xml.fento.repository.exception.RetrieveException;
 
-public interface DocumentRepository {
+public interface DocumentRepository  {
 	
-	Object load(Serializable object) throws RetrieveException;
-
-	void persist(Serializable object, Document document) throws PersistException;
-
-
-	Object template();
-
+	<T extends Serializable>  T load(T object) throws RetrieveException;
+	<T extends Serializable> void persist(T object) throws PersistException;
+	<T extends Serializable> void persist(T object, Document document) throws PersistException;
+	<T extends Serializable> void remove(T object) throws RetrieveException;
+	Document template();
 
 }
