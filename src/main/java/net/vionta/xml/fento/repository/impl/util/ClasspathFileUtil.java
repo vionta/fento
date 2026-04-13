5e9f4669c4719cfd1fb5c01854ec8060807eac6a
Jorge Sanchez <jorge@vionta.net>
2026-04-14 01:10:09 +0200

    Updates

--- src/main/java/net/vionta/xml/fento/repository/impl/util/ClasspathFileUtil.java
+++ src/main/java/net/vionta/xml/fento/repository/impl/util/ClasspathFileUtil.java
@@ -28,7 +28,7 @@ import net.sf.saxon.resource.ResourceLoader;
 public class ClasspathFileUtil {
 
 	
-	static Logger LOGGER = getLogger(FileManager.class);
+	static Logger log = getLogger(FileManager.class);
 	
 	 /**
      * Checks if a file exists in the defined path.
@@ -38,36 +38,18 @@ public class ClasspathFileUtil {
 	 * @throws URISyntaxException 
      */
     public static boolean fileExists(String path)  {
-    	LOGGER.debug("Checking Path "+path);
+    	log.debug("Checking Path "+path);
     	try {
     	URL url = ResourceLoader.class.getClassLoader().getResource(path); 
     	File file = new File(url.getPath());
     	return new File(path).exists();
     	} catch (Exception e) {
-    		LOGGER.error("File  "+path+ " does not exist in classpath ");
-    		LOGGER.debug(e.getCause() +" - "+ e.getMessage() );
+    		log.error("File  "+path+ " does not exist in classpath ");
+    		log.debug(e.getCause() +" - "+ e.getMessage() );
     		return false;
 		}
     }
-	
-    /**
-     * Writes a file, from within the classpath entries. 
-     * Ths method can not write if the file does not exist 
-     * previously on the classpath.
-     *
-     * @param path file relative path.
-     * @param contents File contents.
-     * @throws IOException Write file exception (permissions, etc.).
-     */
-    public static void writeFile(String path, String contents) throws IOException, URISyntaxException  {
-    	LOGGER.debug("Writng File "+path);
-    	URL url = ResourceLoader.class.getClassLoader().getResource(path);
-    	LOGGER.debug("To  "+url);
-    	PrintWriter writer = new PrintWriter(new File(url.getPath()));
-    	writer.println(contents);
-    	writer.close();
-    }
-    
+
     /**
      * Reads a file from the classpath entries
      * 
@@ -79,49 +61,42 @@ public class ClasspathFileUtil {
      * @throws URISyntaxException
      */
     public static String readFile(String path) throws IOException, SAXException, ParserConfigurationException, URISyntaxException  {
-    	LOGGER.debug("Reading file "+path);
+    	log.debug("Reading file "+path);
     	ClassLoader classLoader = ResourceLoader.class.getClassLoader();
         InputStream inputStream = classLoader.getResourceAsStream(path);
         
         byte[] resourceBytes = inputStream.readAllBytes();
-        
-//        InputStream inputStream;
-//    	String contents = ""; 
-//		try {
-//			InputStream inputStream = classLoader.getResourceAsStream(path);
-//			inputStream = Files.newInputStream(path);
-//			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-//			String line = null;
-//			boolean firstLine= true;
-//			while ((line = bufferedReader.readLine()) != null) {
-//				if(firstLine)	contents+=line.substring(1);
-//				else {
-//					contents+=line.substring(1);
-//					firstLine=false;
-//				}
-//				contents+="\n";
-//			}
-//			return contents; 
-//		} catch (IOException e) {
-//			LOGGER.error("Read file failed with cause:");
-//			LOGGER.error(""+e.getCause());
-//			return null; 
-//		}
-        
-        
-        
-        
-        
-        
         inputStream.close();
         // Convert the byte array to a String and print it
         String resourceString = new String(resourceBytes, StandardCharsets.UTF_8);
-        if(resourceString.indexOf("?")==0) resourceString = resourceString.substring(1);
-        LOGGER.debug(" Returning:"+resourceString);
+//        log.debug(" Returning:"+resourceString);
+//        log.debug(" ? > "+resourceString.indexOf("?"));        	
+        if(resourceString.indexOf("<")==1) {
+//        	log.debug(" ? TRUE ");        	
+        	resourceString = resourceString.substring(resourceString.indexOf("<"));
+        	log.debug(" TRUE : "+resourceString.substring(0,20));        	
+        }
+        log.debug(" Returning:"+resourceString);
         return resourceString;
-//		return contents;
 	}        
 	
-    
+	
+    /**
+     * Writes a file, from within the classpath entries. 
+     * Ths method can not write if the file does not exist 
+     * previously on the classpath.
+     *
+     * @param path file relative path.
+     * @param contents File contents.
+     * @throws IOException Write file exception (permissions, etc.).
+     */
+    public static void writeFile(String path, String contents) throws IOException, URISyntaxException  {
+    	log.debug("Writng File "+path);
+    	URL url = ResourceLoader.class.getClassLoader().getResource(path);
+    	log.debug("To  "+url);
+    	PrintWriter writer = new PrintWriter(new File(url.getPath()));
+    	writer.println(contents);
+    	writer.close();
+    }
     
 }
