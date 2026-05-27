bc9d7627d948f4eda4714203aa8326cdbe38acf0
Jorge Sanchez <jorge@vionta.net>
2026-05-27 13:37:30 +0200

    Update 27 May

--- src/main/java/net/vionta/xml/fento/bind/annotation/SerializingMode.java
+++ src/main/java/net/vionta/xml/fento/bind/annotation/SerializingMode.java
@@ -9,10 +9,25 @@ import java.lang.annotation.Target;
 @Target(ElementType.FIELD)
 public @interface SerializingMode {
 
+	// What should we do with new elements, when serializing.
 	public int mode() default CREATE_ON_NOT_EXISTING ; 
 
 	public static final int FAIL_ON_NOT_EXISTING = 1;
 	public static final int CREATE_ON_NOT_EXISTING = 2;
-	public static final int AVOID_ON_NOT_EXISTING = 3;
+	public static final int SKIP_ON_NOT_EXISTING = 3;
+
+	
+	// Collection Strategies
+	public static final int BIND_COLLECTION_BY_KEY = 1;
+	public static final int BIND_COLLECTION_BY_POSITION = 2;
+	public static final int BIND_COLLECTION_APPEND_LAST = 3;
+	public static final int BIND_COLLECTION_APPEND_FIRST = 4;
+	public static final int BIND_COLLECTION_FULL_RESET = 5;
+	
+	// Should we delete elements or just update/add
+	public static final int COLLECTION_DONT_DELETE_UNMATCHED= 1;
+	public static final int COLLECTION_DELETE_UNMATCHED = 2;
+
+	
 	
 }
