00113a1a3e12ed2a145c36e01d5842fb9bf25a50
Jorge Sanchez <jorge@vionta.net>
2026-05-27 13:37:32 +0200

    Update 27 May

--- src/main/java/net/vionta/xml/fento/bind/serialize/util/XPathHelper.java
+++ src/main/java/net/vionta/xml/fento/bind/serialize/util/XPathHelper.java
@@ -1,12 +1,8 @@
 package net.vionta.xml.fento.bind.serialize.util;
 
-import java.util.ArrayList;
+import java.util.Map;
 
-import javax.xml.namespace.NamespaceContext;
 import javax.xml.xpath.XPath;
-
-import net.sf.saxon.pull.NamespaceContextImpl;
-import net.vionta.xml.fento.bind.analyze.map.Namespace;
 
 public class XPathHelper {
 
@@ -15,8 +11,20 @@ public class XPathHelper {
 	 * www.saxonica.com, based XPath 3.1 implementation.
 	 * @return
 	 */
-	public static XPath getXPath() {
+	private static XPath getXPath() {
 		return XPathManager.buildXPath();
+	}
+
+	/**
+	 * Returns the default XPath interpreter, a Saxonica 
+	 * www.saxonica.com, based XPath 3.1 implementation.
+	 * @return A XPath 3.1 implementation based on Saxonica.
+	 */
+	public static XPath getXPath(Map<String, String> namespaceList) {
+		XPath xPath = XPathManager.buildXPath();
+		if(namespaceList!= null ) xPath.setNamespaceContext( 
+				DeserializerHelper.extractNamespacesContext(namespaceList));
+		return xPath;
 	}
 	
 }
