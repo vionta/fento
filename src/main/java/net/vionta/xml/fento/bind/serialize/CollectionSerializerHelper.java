9dbd6b03854ca3e79751a4e0119232f74e8a9976
Jorge Sanchez <jorge@vionta.net>
2026-06-05 01:04:46 +0200

    modificado 5 jun

--- src/main/java/net/vionta/xml/fento/bind/serialize/CollectionSerializerHelper.java
+++ src/main/java/net/vionta/xml/fento/bind/serialize/CollectionSerializerHelper.java
@@ -30,6 +30,7 @@ import net.vionta.xml.fento.bind.annotation.Bind;
 import net.vionta.xml.fento.bind.annotation.SerializingMode;
 import net.vionta.xml.fento.bind.serialize.datamerge.CollectionDataMerge;
 import net.vionta.xml.fento.bind.serialize.datamerge.DataMergeBuilder;
+import net.vionta.xml.fento.bind.serialize.util.XPathHelper;
 import net.vionta.xml.fento.bind.serialize.util.XPathManager;
 import net.vionta.xml.fento.exception.BindingException;
 import net.vionta.xml.fento.exception.ExceptionHelper;
@@ -57,33 +58,22 @@ public final class CollectionSerializerHelper {
 			Node baseNode = mainNode; 
 			CollectionDataMerge dataMerge = DataMergeBuilder.buildDataMerge( currentMapping );
 			// Getting the list objects
-
 			
 			dataMerge = DataMergeBuilder.fillNodes(dataMerge, currentMapping, mainNode);
 			// TODO: Esto debe ser multiple
 			Set entrySet = currentMapping.getCollectionClasses().entrySet();
 
-			
 			// Multiple collection : Pending 
 			if(dataMerge.isMultipleCollection()) {
 				//TODO: Mulitple collection
 				//TODO: Mulitple collection
-
-				
-				
-				
 				
 			// Single collection (only one type of elements.	
 			} else  {		
-				
-				populateMergeObjects(parentObject, propertyName, dataMerge, dataMerge.getKeyNodeParameter());
-//				dataMerge.setKeyNodeParameter(propertyName);
-				
-//				if (dataMerge.getIsDeleteAllowed())
-//				checkAndDeleteNodes(mainNode, dataMerge);
 
 				//Bind Collection by key
 				if (dataMerge.getMappingEstrategy() == SerializingMode.BIND_COLLECTION_BY_KEY) {
+					populateMergeObjects(parentObject, propertyName, dataMerge, dataMerge.getKeyNodeParameter());
 					// Removing not existent nodes.
 					int nodePosition = 0;
 					log.debug(" Getting Objects : " + dataMerge.getObjectItems().size());
@@ -93,10 +83,10 @@ public final class CollectionSerializerHelper {
 						log.debug(" Node Items Size " + dataMerge.getNodeItems().size());
 						CollectionItem currentNode = dataMerge.getNodeItems().get(nodePosition);
 						String nodeKey = currentNode.key;
-//						String nodeKey = extractNodeKey(nodePosition, objectKey, currentNode);
 						// if the current node exists
 						log.debug(" Serializing subproperties " + item.key + " - " + item.object);
 						log.debug(" Serializing subproperties " + currentNode.key + " - " + currentNode.node);
+						
 						if (currentNode != null && currentNode.node != null && objectKey.equals(nodeKey)) {
 							new Serializer().serializeSubproperties((Serializable) item.object, currentNode.node,
 								currentMapping.getMappings(), document);
@@ -118,21 +108,25 @@ public final class CollectionSerializerHelper {
 							} else log.warn("Could not create an element with name :" + dataMerge.getCrateNodeExpression());
 						}
 					}
-				} else  if (dataMerge.getMappingEstrategy() == SerializingMode.BIND_COLLECTION_BY_POSITION) {
+					
+				}   else  if (dataMerge.getMappingEstrategy() == SerializingMode.BIND_COLLECTION_BY_POSITION) {
+//					try {
+					populateMergeObjects(parentObject, propertyName, dataMerge);
 					// Removing not existent nodes.
 					int nodePosition = 0;
 					log.debug(" Getting Objects : " + dataMerge.getObjectItems().size());
 					//TODO: For complicated mappings the mainNode may need to be adjusted. 
 					for (CollectionItem item : dataMerge.getObjectItems()) {
-						String objectKey = item.key;
-						CollectionItem currentNode = dataMerge.getNodeItems().get(nodePosition);
-						String nodeKey = extractNodeKey(nodePosition, objectKey, currentNode);
-						// if the current node exists
-						if (currentNode != null && currentNode.node != null && objectKey.equals(nodeKey)) {
-							log.debug(" Serializing subproperties " + item.object + " - " + currentNode.node);
-							new Serializer().serializeSubproperties((Serializable) item.object, currentNode.node,
-								currentMapping.getMappings(), document);
-							nodePosition++;
+//						String objectKey = item.key;
+						log.debug(" Getting Node: " + nodePosition + " From : " + dataMerge.getNodeItems().size());
+						if( nodePosition < dataMerge.getNodeItems().size() ) {
+							CollectionItem currentNode = dataMerge.getNodeItems().get(nodePosition);
+							if (currentNode != null && currentNode.node != null ) {
+								log.debug(" Serializing subproperties " + item.object + " - " + currentNode.node);
+								new Serializer().serializeSubproperties((Serializable) item.object, currentNode.node,
+									currentMapping.getMappings(), document);
+								nodePosition++;
+							}
 						} else {
 							log.debug(" Node could not be directly seriaziled, checking case ");
 							// review this part with multiple collections.
@@ -141,17 +135,21 @@ public final class CollectionSerializerHelper {
 								log.debug(" Creating node : " + elementCreated);
 								Element newElement = document.createElement(elementCreated);
 								log.debug(" Creating node : " + elementCreated);
-								if (currentNode.node != null)
-									mainNode.insertBefore(currentNode.node, newElement);
+//								if (currentNode.node != null)
+//									mainNode.insertBefore(currentNode.node, newElement);
 								// If there is no following node
-								else mainNode.appendChild(newElement);
+//								else 
+									mainNode.appendChild(newElement);
 								new Serializer().serializeSubproperties((Serializable) item.object, newElement,
 									currentMapping.getMappings(), document);
 							} else log.warn("Could not create an element with name :" + dataMerge.getCrateNodeExpression());
 						}
 					}
 					
-					
+//					} catch (Exception e) {
+//						e.printStackTrace();
+//						
+//					}
 					
 				} //else  if (dataMerge.getMappingEstrategy() == SerializingMode.BIND_COLLECTION_FULL_RESET) {
 				 
@@ -191,6 +189,24 @@ public final class CollectionSerializerHelper {
 		dataMerge.setObjectItems(listObjects, keyPropertyName);
 		return dataMerge;
 	}
+	
+	/**
+	 * Populates the data Merge with the actual collection objects without setting the key
+	 * @param parentObject The actual java object (dto).
+	 * @param propertyName The property name of the key value
+	 * @param dataMerge The collection data.
+	 * @return Collection Data with the included objects.
+	 * @throws NoSuchMethodException 
+	 * @throws InvocationTargetException 
+	 * @throws IllegalAccessException 
+	 * @throws MappingException 
+	 */
+	private static CollectionDataMerge populateMergeObjects(Serializable parentObject, String propertyName,
+			CollectionDataMerge dataMerge) throws IllegalAccessException, InvocationTargetException, NoSuchMethodException  {
+		ArrayList listObjects = (ArrayList) PropertyUtils.getNestedProperty(parentObject, propertyName);
+		dataMerge.setObjectItemsWithoutKey(listObjects);
+		return dataMerge;
+	}
 
 	/**
 	 * Extract the noe key
@@ -200,10 +216,10 @@ public final class CollectionSerializerHelper {
 	 * @return
 	 * @throws MappingException
 	 */
-	private static String extractNodeKey(int nodePosition, String objectKey, CollectionItem currentNode) throws MappingException {
+	private static String extractNodeKey(int nodePosition, String objectKey, CollectionItem currentNode, Map<String, String> namespaces) throws MappingException {
 		String nodeValue ="";
 		try {
-			nodeValue = getXPath().evaluate((String) objectKey, currentNode.node);
+			nodeValue = XPathHelper.getXPath(namespaces).evaluate((String) objectKey, currentNode.node);
 			log.debug(" Returning Node key " + nodeValue);
 			if (nodeValue == null)
 				nodeValue = "";
@@ -224,10 +240,10 @@ public final class CollectionSerializerHelper {
 	 * @return
 	 * @throws XPathExpressionException 
 	 */
-	public static String getKeyValue(Node node, String expression ) throws XPathExpressionException {
+	public static String getKeyValue(Node node, String expression, Map<String, String> namespaces ) throws XPathExpressionException {
 		if(expression == null  || expression == "" || node == null ) return "";
 		String nodeKey = (String) 
-				getXPath().evaluate(expression, node,XPathConstants.STRING);
+				XPathHelper.getXPath(namespaces).evaluate(expression, node,XPathConstants.STRING);
 		return nodeKey;
 	}
 
