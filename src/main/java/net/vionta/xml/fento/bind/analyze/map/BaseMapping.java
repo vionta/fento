b9cbe20a51756b5ca32bb4f16acc418439b5c540
Jorge Sanchez <jorge@vionta.net>
2026-06-05 01:04:44 +0200

    modificado 5 jun

--- src/main/java/net/vionta/xml/fento/bind/analyze/map/BaseMapping.java
+++ src/main/java/net/vionta/xml/fento/bind/analyze/map/BaseMapping.java
@@ -2,7 +2,6 @@ package net.vionta.xml.fento.bind.analyze.map;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import net.vionta.xml.fento.bind.annotation.DeserializingMode;
